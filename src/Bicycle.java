20e9d8a2e1adbce4ddeb174bba75ea22c51857c4
Lior1254 <liorshemtov2008@gmail.com>
2024-10-28 15:24:40 +0200

    Initial commit

---
+++ src/Bicycle.java
@@ -0,0 +1,18 @@
+public class Bicycle extends Vehicle
+{
+
+    @Override
+    public void accelerate() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car extends Vehicle
+{
+
+    @Override
+    public void accelerate() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
